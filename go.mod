598fbe57f952a3ea80980dc030a85963e8ee2bbb
wangkechun <hi@hi-hi.cn>
2019-04-08 10:21:56 +0800

    support retry

--- go.mod
+++ go.mod
@@ -11,5 +11,6 @@ require (
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/pflag v1.0.3 // indirect
 	github.com/stretchr/testify v1.3.0
+	golang.org/x/net v0.0.0-20180826012351-8a410e7b638d
 	google.golang.org/grpc v1.19.1
 )
--- pkg/process/server_instance.go
+++ pkg/process/server_instance.go
@@ -1,23 +1,25 @@
 package process
 
 import (
+	"context"
 	"log"
 	"sync"
+	"time"
 
-	"github.com/mattn/go-shellwords"
 	"github.com/pkg/errors"
 	pb "github.com/wangkechun/gosupervisor/pkg/proto"
 )
 
 const ServiceVersion = "v0.1"
+const monitorInterval = time.Second
 
 type serverInstance struct {
 	config  *pb.ConfigFile
-	process map[string]*procesInstances
+	process map[string]*processInstances
 	lock    sync.RWMutex
 }
 
-func (s *serverInstance) load() error {
+func (s *serverInstance) initLoad() error {
 	if s.config.Version != ServiceVersion {
 		return errors.Errorf("unknow version %v", s.config.Version)
 	}
@@ -25,30 +27,57 @@ func (s *serverInstance) load() error {
 		name := v.ProcessName
 		_, ok := s.process[name]
 		if ok {
-			return errors.Errorf("find repeat process %v", name)
+			return errors.Errorf("find duplicate process %v", name)
 		}
-		p := procesInstances{spec: *v, status: pb.ProcessStatus{}}
-		args, err := shellwords.Parse(v.Command)
+		p, err := newProcessInstances(v)
 		if err != nil {
-			return errors.Errorf("shell command is incorrect, name:%v err:%v command:%v", name, err, v.Command)
+			return errors.Wrap(err, "newProcessInstances")
 		}
-		if len(args) == 0 {
-			return errors.Errorf("shell command empty")
-		}
-		p.args = args
-		s.process[name] = &p
+		s.process[name] = p
 	}
 	return nil
 }
 
-func (s *serverInstance) startAll() {
+func (s *serverInstance) initStartAll() {
 	for _, v := range s.process {
-		go func(v *procesInstances) {
-			err := v.start()
+		go func(v *processInstances) {
+			err := v.start(startByAuto)
 			if err != nil {
 				log.Printf("start process:%v, err:%v", v.spec.ProcessName, err)
 			}
 		}(v)
+	}
+}
+
+func (s *serverInstance) monitor() {
+	s.lock.RLock()
+	process := make([]*processInstances, 0)
+	for _, v := range s.process {
+		status := v.readStatus()
+		if status.Status.Status == pb.ProcessStatus_BACKOFF || status.Status.Status == pb.ProcessStatus_EXITED {
+			process = append(process, v)
+		}
+	}
+	s.lock.RUnlock()
+
+	for _, process := range process {
+		log.Println("monitor try start process", process.spec.ProcessName)
+		err := process.start(startByMonitor)
+		if err != nil {
+			log.Println("monitor try start process failed", process.spec.ProcessName, err)
+		}
+	}
+}
+
+func (s *serverInstance) initRunMonitor(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+		}
+		s.monitor()
+		time.Sleep(monitorInterval)
 	}
 }
 
